a56246f6dbae3808a5502181fd324f3a8dccea25
Abul Kalam Azad <you@example.com>
2021-04-07 23:03:29 +0600

    latest changes

--- js/main.js
+++ js/main.js
@@ -7,12 +7,16 @@ const paths = document.querySelectorAll('path');
 const tree = document.querySelector('.tree');
 const leftCol = document.querySelector('.left-col');
 const rightCol = document.querySelector('.right-col');
+const bannerTitle = document.querySelector('.banner-title');
+const biomeIMG = document.querySelector('.biome-img');
 
 // Hiding images on load
 
 tree.style.opacity = 1;
 leftCol.style.opacity = 1;
 rightCol.style.opacity = 1;
+bannerTitle.style.opacity = 1;
+biomeIMG.style.opacity = 1;
 
 const tl = gsap.timeline({
     defaults: { duration: 1, ease: Back.easeOut.config(2), opacity: 0 },
@@ -45,25 +49,30 @@ tl.from('.biome', {
 tl.from('.left-col', {
     scrollTrigger: {
         trigger: '.anim-container',
+        start: 'top 10%',
         pin: true,
         // markers: true,
         scrub: true,
     },
     ease: 'power1.out',
     opacity: 0,
-    y: 200,
+    y: 100,
 });
 
 if (window.innerWidth > 376) {
     tl.from('.right-col', {
         scrollTrigger: {
-            trigger: '.anim-container',
-            pin: true,
+            trigger: '.left-col',
+            // pin: true,
+            start: 'top 20%',
             // markers: true,
             scrub: true,
         },
         ease: 'power1.out',
         opacity: 0,
-        y: 200,
+        y: 100,
     });
 }
+
+gsap.from('.banner-title', 0.9, { y: 150, opacity: 0 });
+gsap.from('.biome-img', 0.45, { y: 100, opacity: 0, delay: 0.9 });

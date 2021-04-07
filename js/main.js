fec73965ac2a1ddc4de7cfcee13a8fcc816cb693
Abul Kalam Azad <you@example.com>
2021-04-07 15:11:29 +0600

    staggered effect added

---
+++ js/main.js
@@ -0,0 +1,38 @@
+gsap.registerPlugin(ScrollTrigger);
+const tl = gsap.timeline({
+    defaults: { duration: 1, ease: Back.easeOut.config(2), opacity: 0 },
+});
+
+// tl.to('.tree', {
+//     scrollTrigger: {
+//         trigger: '.anim-container',
+//         pin: true,
+//         // markers: true,
+//         scrub: true,
+//     },
+// });
+
+tl.from('path', {
+    opacity: 0,
+    stagger: {
+        each: 0.05,
+        from: 'edges',
+        grid: 'auto',
+    },
+    scrollTrigger: {
+        trigger: '.anim-container',
+        pin: true,
+        // markers: true,
+        scrub: true,
+    },
+});
+
+tl.from('.biome', 2, {
+    scrollTrigger: {
+        trigger: '.anim-container',
+        pin: true,
+        // markers: true,
+        scrub: true,
+    },
+    opacity: 1,
+});

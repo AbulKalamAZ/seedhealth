22297761ef33bee4ea372ecb033ff5357db2f602
Abul Kalam Azad <you@example.com>
2021-04-08 12:41:02 +0600

    all issues are fixed

--- js/main.js
+++ js/main.js
@@ -15,64 +15,62 @@ const biomeIMG = document.querySelector('.biome-img');
 tree.style.opacity = 1;
 leftCol.style.opacity = 1;
 rightCol.style.opacity = 1;
-bannerTitle.style.opacity = 1;
 biomeIMG.style.opacity = 1;
 
-const tl = gsap.timeline({
-    defaults: { duration: 1, ease: Back.easeOut.config(2), opacity: 0 },
-});
+// Let's get our hands dirty
 
-tl.from('path', {
-    opacity: 0,
-    stagger: {
-        each: 0.5,
-        from: 'center',
-        grid: 'auto',
-    },
-    scrollTrigger: {
-        trigger: '.anim-container',
-        pin: true,
-        // markers: true,
-        scrub: true,
-    },
-});
+const timeline = gsap.timeline({ defaults: { duration: 0.7 } });
 
-tl.from('.biome', {
-    scrollTrigger: {
-        trigger: '.anim-container',
-        pin: true,
-        scrub: true,
-    },
-    opacity: 1,
-});
-
-tl.from('.left-col', {
-    scrollTrigger: {
-        trigger: '.anim-container',
-        start: 'top 10%',
-        pin: true,
-        // markers: true,
-        scrub: true,
-    },
-    ease: 'power1.out',
-    opacity: 0,
-    y: 100,
-});
-
-if (window.innerWidth > 376) {
-    tl.from('.right-col', {
+timeline
+    .to('.banner-title', { y: 0, opacity: 1 })
+    .from('.biome-img', { y: 100, opacity: 0 }, '<.7')
+    .from('path', {
+        opacity: 0,
+        stagger: {
+            each: 0.5,
+            from: 'center',
+            grid: 'auto',
+        },
         scrollTrigger: {
-            trigger: '.left-col',
+            trigger: '.anim-container',
+            pin: true,
+            // markers: true,
+            scrub: true,
+        },
+    })
+    .to('.biome', {
+        scrollTrigger: {
+            trigger: '.anim-container',
+            pin: true,
+            scrub: true,
+        },
+        opacity: 0,
+    })
+    .from('.left-col', {
+        scrollTrigger: {
+            trigger: '.anim-container',
+            start: 'top 10%',
             // pin: true,
-            start: 'top 20%',
             // markers: true,
             scrub: true,
         },
         ease: 'power1.out',
         opacity: 0,
         y: 100,
-    });
-}
-
-gsap.from('.banner-title', 0.9, { y: 150, opacity: 0 });
-gsap.from('.biome-img', 0.45, { y: 100, opacity: 0, delay: 0.9 });
+    })
+    .from(
+        '.right-col',
+        {
+            scrollTrigger: {
+                trigger: '.left-col',
+                // pin: true,
+                start: 'top 10%',
+                // markers: true,
+                scrub: true,
+            },
+            ease: 'power1.out',
+            opacity: 0,
+            y: 100,
+        },
+        '<.5'
+    );
